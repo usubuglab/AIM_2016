c87551ee8c53ff61a3c3bdc7c15902e714f0ca96
NicoleCappuccio <Nicole.Cappuccio@gmail.com>
2013-12-17 11:36:44 -0700

    trial upload 1

---
+++ NC_DataConsumption_WRSAdb - Copy.r
@@ -0,0 +1,260 @@
+#-------------------------------------------------------INPUTS--------------------------------------------------------#
+#In the ideal world, users should only need to put inputs here and be able to get results out of the 'black box' below using existing functions.
+DBpassword=''#Always leave blank when saving for security and because changes annually. Contact Sarah Judson for current password.
+
+#FILTERS
+##from most to least specific
+sitecodes=c('AR-LS-8003','AR-LS-8007', 'TP-LS-8240')
+dates=c('05/05/2005')
+hitchs=c('')#NOT WORKING YET
+crews=c('R1')#NOT WORKING YET
+projects=c('NRSA')#NOT WORKING YET
+
+#PARAMETERS
+#specify if desired (will make queries less intensive):
+testP=c('ANGLE','APPEALING','ALGAE')#test, one from each level of table
+bankP=c('ANGLE','UNDERCUT','EROSION','COVER','STABLE')
+
+
+#--------------------------------------------------------SETUP--------------------------------------------------------#
+#LOAD required packages#
+requiredPACKAGES=c('reshape', 'RODBC')
+for (r in 1:length(requiredPACKAGES)){
+  if ((requiredPACKAGES[r] %in% installed.packages()[,1])==FALSE){install.packages(requiredPACKAGES[r])}#auto-install if not present
+  library(requiredPACKAGES[r],character.only = TRUE)
+}
+
+#setwd dynamically ##SWJ to do!
+
+##Establish an ODBC connection##
+#the db was created in SQL Server Manager on 11/19/2013 by Sarah Judson#
+wrsaConnectSTR=sprintf("Driver={SQL Server Native Client 10.0};Server=129.123.16.13,1433;Database=WRSAdb;Uid=feng; Pwd=%s;",DBpassword)
+wrsa1314=odbcDriverConnect(connection = wrsaConnectSTR)
+
+
+#SQL assistance functions
+#inLOOP: concatenate list objects into an "IN" string for insertion into queries
+inLOOP=function(inSTR) {
+  inSTR=unlist(inSTR)
+  for (i in 1:length(inSTR)){
+    comma=ifelse(i==length(inSTR),'',',')
+    STRl=sprintf("'%s'%s",inSTR[i],comma)
+    if(i==1){loopSTR=STRl} else{loopSTR=paste(loopSTR,STRl)}
+  }   
+  return(loopSTR) 
+}
+#tblRetrieve: standard retrieval query
+tblRetrieve=function(table, parameters=''){
+  if(parameters==''){parameters=sqlQuery(wrsa1314,sprintf("select distinct parameter from %s", table))}
+  sqlTABLE=sqlQuery(wrsa1314, sprintf('select * from %s where UID in (%s) and parameter in (%s)',table, inLOOP(UIDs),inLOOP(parameters)))
+  return(sqlTABLE)#could auto return the pivoted view, but currently assuming that is for on the fly viewing and is not the easiest way to perform metrics
+}
+#PVTconstruct: dyanamic generation of SQL pivot queries
+PVTconstruct=function(parameters=c('SITE_ID'), tblTYPE=tblVERIFICATION, filter=''){
+  tblPVTstr="select UID %s, %s
+FROM (SELECT UID %s,Parameter, Result
+FROM %s) p PIVOT (min(Result) FOR Parameter IN (%s)) AS pvt
+WHERE %s
+"
+  if(tblTYPE %in% c('tblVERIFICATION', 'tblREACH')){ParamResolution=''
+  } else if(tblTYPE=='tblTRANSECT'){ParamResolution=',Transect'
+  } else if(tblTYPE=='tblPOINT'){ParamResolution=',Transect, Point'
+  } else{ParamResolution=''}
+  for (p in 1:length(parameters)){
+    comma=ifelse(p==length(parameters),'',',')
+    Pas=sprintf("[%s] AS %s%s",parameters[p],parameters[p],comma)
+    Psq=sprintf("[%s]%s",parameters[p],comma)
+    if(p==1){loopPas=Pas; loopPsq=Psq} else{loopPas=paste(loopPas,Pas); loopPsq=paste(loopPsq,Psq)}
+  }
+  sqlTEXT=sprintf(tblPVTstr, ParamResolution, loopPas, ParamResolution,tblTYPE,loopPsq,filter)
+  return(sqlTEXT)
+}
+
+
+
+
+#--------------------------------------------------------SQL RETRIEVE--------------------------------------------------------#
+#SQL tables are created and imports managed via an independent R scripts (createNRSA1314db_SWJ.r)
+#all possible tables
+sqlTables(wrsa1314)
+sqlColumns(wrsa1314,"tblPOINT")
+
+#select samples
+UIDs=sqlQuery(wrsa1314, sprintf("select distinct UID from tblVERIFICATION 
+                                where (active='TRUE') 
+                                AND ((Parameter='SITE_ID' and Result in (%s)) OR (Parameter='DATE_COL' and Result in (%s)))"
+                                ,inLOOP(sitecodes),inLOOP(dates)))
+#SWJ to do: add additional filters
+#SWJ to do: prompt for data entry (mini-GUI)
+
+
+#retrieve desired tables
+#EXAMPLES of tblRetrieve function# (note: parameter lists were specified in the "Inputs" section at the beginning of this script)
+tblREACH=tblRetrieve('tblREACH')#not specifying parameters will retrieve the entire table
+tblPOINT=tblRetrieve('tblPOINT')
+tblTRANSECT=tblRetrieve('tblTRANSECT')
+tblVERIFICATION=tblRetrieve('tblVERIFICATION')
+tblMETADATA= sqlQuery(wrsa1314, "select * from tblMETADATA")
+tblREACHtest=tblRetrieve('tblREACH',testP)
+tblPOINTbank=tblRetrieve('tblPOINT',bankP)
+
+
+
+
+#Close ODBC connection when done talking to SQL Server
+odbcClose(wrsa1314)
+
+#--------------------------------------------------------PIVOT VIEWS--------------------------------------------------------#
+##RESHAPE to PIVOT## 
+#EXAMPLES of both methods#
+#SQL option ('View' creation to copy/paste)
+bankPVTstr=PVTconstruct(parameters=bankP,tblTYPE='tblPOINT', filter="POINT in ('LF','RT')");print(bankPVTstr)#- need permission from Sarah Judson and to reopen ODBC before saving Views for permanent use in SQL Server
+  #retrieve said query from SQL
+    wrsa1314_2=odbcDriverConnect(connection = wrsaConnectSTR)
+    tblPOINTbankPVTs=sqlQuery(wrsa1314_2,bankPVTstr)
+    odbcClose(wrsa1314_2)
+#R option (cast)
+tblPOINTbankPVTr=cast(subset(tblPOINTbank,select=c(UID, TRANSECT,POINT,PARAMETER,RESULT)), UID + TRANSECT + POINT ~ PARAMETER)#very predictable structure except for the input table and whether transect and point need to be included in the columns = possibly plug into function
+
+
+#--------------------------------------------------------ANALYSIS--------------------------------------------------------#
+##AGGREGATION##
+#EXAMPLES#
+#count number of records per parameter to check for missing data
+qastatsBANK_CNTcast=cast(tblPOINTbank, UID ~ PARAMETER, value='RESULT', fun.aggregate=length)#should this filter out NULLs or flags? does EPA write a line for each record even if no value recorded?
+qastatsBANK_CNTagg=aggregate(tblPOINTbank,FUN='length', by=list(tblPOINTbank$UID,tblPOINTbank$TRANSECT,tblPOINTbank$POINT))
+#cast seems like the more elegant solution
+#convert numerics before performing stats
+tblPOINTbankNUM=subset(tblPOINTbank,subset= is.na(as.numeric(as.character(tblPOINTbank$RESULT)))==FALSE);tblPOINTbankNUM$RESULT=as.numeric(as.character(tblPOINTbankNUM$RESULT))
+qastatsBANK_MEANcast=cast(tblPOINTbankNUM, UID ~ PARAMETER, value='RESULT', fun.aggregate=mean)
+
+##GRTS adjusted weights##
+#TBD# Pull from UTBLM
+
+##EPA aquamet##
+#TBD# Pull from aquamet 1.0 provided by Tom Kincaid and Curt Seegler via Marlys Cappaert
+
+##OE computation##
+#TBD# Pull from VanSickle
+
+#Predicted WQ##
+#TBD#  Pull from UTBLM, John Olson/Ryan Hill
+
+##NMDS##
+#TBD#
+
+##GIS connections##
+#TBD#
+
+
+#--------------------------------------------------------REPORTING--------------------------------------------------------#
+##Figures and Tables##
+#TBD# Pull from UTBLM
+
+##SWEAVE##
+#TBD#
+
+##BibTex##
+#TBD#
+
+
+#--------------------------------------------------------sarah's Gibberish-------------------------------------------------------#
+# #pseudocode - consume data from flat db
+# #ODBC connection to SQL server WRSAdb
+# #import via SQL string call - include filters on data (i.e. hitch, project, crew)
+# #mash (merge) tables (?) OR pvt for viewing (?) -- SQL: view, Access: Query
+# ##ex (old): merge(EVENT3,subset(original,select=selectCOLparse),by="SampleID")
+# ##demonstrate complexity of calling by column name vs. parameter text in both SQL and R
+# ###SQL: filter query --> possibly PIVOT to view --> aggregate query
+# ###R: filter strings, apply across multiple --> PVT to view --> aggregate OR run predefined(EPA,R)/custom functions
+# ###common: convert numerics
+# ###differences: null handling, reproducability and documentation
+# ###leaning (SWJ): R for dynamic queries/code, reproducability and 'instant' documentation; in either mode, PIVOTS should be treated as temporary views for scanning data, not basis for subsequent queries because they will then be tied to column names 
+# ##ex: library('reshape'); cast(data, x~y)
+# #separate numbers and characters (will R autodetect?) -- SQL: Cast/Convert
+# #filter by parameter and run metric  -- SQL: sub-queries
+# ##ex: subset(tblPOINT, subset=Parameter=='Angle')
+# ##could set it up so that user doesn't even need to which table
+# ##set up to easily call the parameters table and other metadata (crew, hitch) tables --> will we store crew and hitch info in sampletracking access or SQL server?
+# #aggregate by site and crew  -- SQL: group by (aggregate) query
+# ##ex (old): aggregate(x=as.numeric(sampDATAin$SampleID),FUN=agg3,by=list(sampDATAin$SamplingEvent,sampDATAin$Station,sampDATAin$WaterYear))
+# #report -- R SWEAVE vs. Access report vs. Crystal Reports
+# 
+# #check for existing packages
+# #install.packages('reshape')
+# library('reshape')
+# 
+# #establish an ODBC connection#
+# #the db was created in SQL Server Manager on 11/19/2013
+# #manually set up the database (WRSAdb) and the odcb connection (WRSAconnect)
+# library("RODBC")
+# user='feng'
+# #ENTER DB PASSWORD
+# print ("Please enter Password")
+# password='Something~Clever!@'#("Enter Password")#raw_input() in python, not sure of R equivalent #http://rosettacode.org/wiki/Dynamic_variable_names#R
+# nrsa1314<-odbcConnect("WRSAconnect",uid=user,pwd=password)
+# #SQL assistance functions
+# #inLOOP: concatenate list objects into an "IN" string for insertion into queries
+# inLOOP=function(inSTR) {
+#   inSTR=unlist(inSTR)
+#   for (i in 1:length(inSTR)){
+#     comma=ifelse(i==length(inSTR),'',',')
+#     STRl=sprintf("'%s'%s",inSTR[i],comma)
+#     if(i==1){loopSTR=STRl} else{loopSTR=paste(loopSTR,STRl)}
+#   }   
+#   return(loopSTR) 
+# }
+# #tblRetrieve: standard retrieval query
+# tblRetrieve=function(table, parameters=''){
+#   if(parameters==''){parameters=sqlQuery(nrsa1314,sprintf("select distinct parameter from %s", table))}
+#   sqlTABLE=sqlQuery(nrsa1314, sprintf('select * from %s where UID in (%s) and parameter in (%s)',table, inLOOP(UIDs),inLOOP(parameters)))
+#   return(sqlTABLE)#could auto return the pivoted view, but currently assuming that is for on the fly viewing and is not the easiest way to perform metrics
+# }
+# 
+# 
+# #FILTERS
+# ##from most to least specific
+# sitecodes=c('AR-LS-8003','AR-LS-8007', 'TP-LS-8240')
+# dates=c('05/05/2005')
+# hitchs=c('')
+# crews=c('R1')
+# projects=c('NRSA')
+# 
+# 
+# 
+# #select samples
+# UIDs=sqlQuery(nrsa1314, sprintf("select distinct UID from tblVERIFICATION 
+#                                 where (active='TRUE') 
+#                                 AND ((Parameter='SITE_ID' and Result in (%s)) OR (Parameter='DATE_COL' and Result in (%s)))"
+#                                 ,inLOOP(sitecodes),inLOOP(dates)))
+# #SWJ to do: add additional filters
+# #SWJ to do: prompt for data entry (mini-GUI)
+# 
+# #PARAMETERS
+# #specify if desired (will make queries less intensive):
+# testP=c('ANGLE','APPEALING','ALGAE')#test, one from each level of table
+# bankP=c('ANGLE','UNDERCUT','EROSION','COVER','STABLE')
+# 
+# #retrieve desired tables
+# tblREACH=tblRetrieve('tblREACH')#not specifying parameters will retrieve the entire table
+# tblREACHtest=tblRetrieve('tblREACH',testP)
+# tblPOINTbank=tblRetrieve('tblPOINT',bankP)
+# 
+# #pivot tables for viewing
+# tblPOINTbankPVT=cast(subset(tblPOINTbank,select=c(UID, TRANSECT,POINT,PARAMETER,RESULT)), UID + TRANSECT + POINT ~ PARAMETER)#very predictable structure except for the input table and whether transect and point need to be included in the columns = possibly plug into function
+# 
+# #further subset data in custom ways
+# 
+# #compute aggregate statistics
+# #count number of records per parameter to check for missing data
+# qastatsBANK_CNTcast=cast(tblPOINTbank, UID ~ PARAMETER, value='RESULT', fun.aggregate=length)#should this filter out NULLs or flags? does EPA write a line for each record even if no value recorded?
+# qastatsBANK_CNTagg=aggregate(tblPOINTbank,FUN='length', by=list(tblPOINTbank$UID,tblPOINTbank$TRANSECT,tblPOINTbank$POINT))
+# #cast seems like the more elegant solution
+# #convert numerics before performing stats
+# tblPOINTbankNUM=subset(tblPOINTbank,subset= is.na(as.numeric(as.character(tblPOINTbank$RESULT)))==FALSE);tblPOINTbankNUM$RESULT=as.numeric(as.character(tblPOINTbankNUM$RESULT))
+# qastatsBANK_MEANcast=cast(tblPOINTbankNUM, UID ~ PARAMETER, value='RESULT', fun.aggregate=mean)
+# 
+# #plugging into aquamet
+# 
+# #end ODBC connection#
+# odbcClose(nrsa1314)
