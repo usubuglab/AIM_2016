2b0645a3df0f9c91799506a1b05763d7385321aa
Jennifer Courtwright <jennifer.courtwright@usu.edu>
2018-04-03 08:14:07 -0600

    added a bunch of box plot stuff to the end and changed a couple other settings for 2017 data

--- Indicator_Export.R
+++ Indicator_Export.R
@@ -13,11 +13,12 @@
 #IndicatorCheck=read.csv('Z:\\buglab\\Research Projects\\AIM\\Analysis\\QC\\2017\\priority projects\\IndicatorCheck7Nov2017.csv')
 #IndicatorCheck=read.csv('Z:\\buglab\\Research Projects\\AIM\\Projects\\Idaho\\Statewide\\Analysis\\Weights_ExtentEstimates\\IndicatorCheckIDstatewidedata_13April2017.csv')
 #IndicatorCheck=read.csv('Z:\\buglab\\Research Projects\\AIM\\Projects\\Utah\\GSENM\\Analysis\\Weights_ExtentEstimates\\IndicatorCheckGrandStaircase_18April2017.csv')
-IndicatorCheck=read.csv('Z:\\buglab\\Research Projects\\AIM\\Analysis\\QC\\2017\\priority projects\\IndicatorCheckSalmon19Dec2017.csv')
+IndicatorCheck=read.csv('Z:\\buglab\\Research Projects\\AIM\\Analysis\\QC\\2017\\IndicatorCheckAll14march2018_allsites.csv')
 
 ###Add spatial attributes or Misc other categorical fields needed for setting benchmarks
 #size class info
 IndicatorCheck$BNK_THRESH=ifelse(as.numeric(IndicatorCheck$XBKF_W_CHECK)>10,"LargeWadeable","SmallWadeable")
+
 
 #get Ecoregions
 #new design database
@@ -27,8 +28,10 @@ SiteInfo=join(GISInfo,DesignInfo,by="MS_ID")
 Indicators=join(IndicatorCheck, SiteInfo, by="SITE_ID_CHECK",type="left",match="first")
 
 #join ecoregions, size class, and protocol info
-Indicators$NAMC_Benchmark=paste(Indicators$Ecoregion_spelledout,Indicators$BNK_THRESH, sep="_")
 Indicators$BNK_THRESH=ifelse(Indicators$PROTOCOL2_CHECK=='BOATABLE',"BOATABLE",Indicators$BNK_THRESH)
+Indicators$NAMC_Benchmark=paste(Indicators$Ecoregion_spelledout,Indicators$BNK_THRESH, sep="_")
+#need to add some code here to get boating sites lumped for benchmark tool
+
 Indicators$THRESH=paste(Indicators$ECO10,Indicators$BNK_THRESH, sep="_")#2011-2015#works with new design database as well
 Indicators$THRESH3=as.factor(Indicators$THRESH)
 levels(Indicators$THRESH3) <- list( XE_SOUTH_SmallWadeable="XE_SOUTH_SmallWadeable",XE_SOUTH_LargeWadeable="XE_SOUTH_LargeWadeable", 
@@ -53,7 +56,7 @@ Indicators$THRESH4=ifelse(Indicators$ECO10=="XE_EPLAT"|Indicators$ECO10=="XE_SOU
 #If joining bug data to data already in benchmark tool or AquADat just run this portion
 Bugs=read.csv("\\\\share1.bluezone.usu.edu\\miller\\buglab\\OE_Modeling\\NAMC_Supported_OEmodels\\final_bug_scores_R_input_join_to_all_other_metrics_statebasedscores.csv")
 #subset so that only the state model or the model of interest is in the data...this is necesssary because this file has westwide as well as state based scores for a single sample
-Bugs=subset(Bugs,MODEL=="ColumbiaRiverBasin_PIBO")#Westwide, ColumbiaRiverBasin_PIBO, OR_WesternCordillera_ColumbiaPlateau, OR_MarineWesternCoastalForest, UT_DEQ_2015,CO_EDAS-Biotype1, CO_EDAS-Biotype2,CO_EDAS-Biotype3,CA_CSCI,NV_MMI
+Bugs=subset(Bugs,MODEL!="Westwide")#Westwide, ColumbiaRiverBasin_PIBO, OR_WesternCordillera_ColumbiaPlateau, OR_MarineWesternCoastalForest, UT_DEQ_2015,CO_EDAS-Biotype1, CO_EDAS-Biotype2,CO_EDAS-Biotype3,CA_CSCI,NV_MMI
 #exclude sites that were outside the experience of the model and counts less than 200 if condition is poor but include everything in AquDat and Benchmark tool export
 Bugs=Bugs[,c(1,12,5,6,7,8,10,11)]
 #write.csv(Bugs,'Bugs.csv') #uncomment this line to export bug data that needs to be added to already existing records in AquADat
@@ -91,6 +94,209 @@ data.xwalk=read.csv('\\\\share1.bluezone.usu.edu\\miller\\buglab\\Research Proje
 data.input=Indicators
 IndicatorsFinal=indicatorXwalk(data.input,data.xwalk)
 
-write.csv(IndicatorsFinal,'IndicatorsFinalExport25Jan2018.csv',na="")     
+write.csv(IndicatorsFinal,'IndicatorsFinalExport15March2018.csv',na="")     
 
 
+#QC computed indicators by looking at boxplots by indicator and ecoregion
+
+IndicatorsFinal=read.csv("Z:\\buglab\\Research Projects\\AIM\\Analysis\\QC\\2017\\AK\\IndicatorsAK8feb2018_Alldata_boxplots_for_workshop.csv")
+IndicatorsFinal=read.csv("Z:\\buglab\\Research Projects\\AIM\\Analysis\\QC\\2017\\AK\\IndicatorsAK8feb2018_Alldata_boxplots_for_workshop.csv")
+
+
+#IndicatorsFinal=read.csv("Z:\\buglab\\Research Projects\\AIM\\Analysis\\QC\\2017\\AK\\AK_points_ecoregions.csv")
+#IndicatorsFinal$NAMC_Benchmark=IndicatorsFinal$US_L3NAME
+str(IndicatorsFinal)
+
+library (ggplot2)
+
+IndicatorsFinalnum=IndicatorsFinalnum[,c(56:57,1:55)]
+
+#for indicator by ecoregion & targeted
+ecoregions=unique(unlist(IndicatorsFinal$EcoregionHybrid10))
+for (s in 2:length(ecoregions)) {
+  Ecoregionsubset=subset(IndicatorsFinal,EcoregionHybrid10==ecoregions[s])
+nums=sapply(Ecoregionsubset,is.numeric)
+IndicatorsFinalnum=Ecoregionsubset[,nums]
+IndicatorsFinalnum$EcoregionHybrid10=Ecoregionsubset$EcoregionHybrid10
+IndicatorsFinalnum$Targeted=Ecoregionsubset$Targeted
+  for (f in 10:length(colnames(IndicatorsFinalnum))-2){
+  png(file=paste(ecoregions[s], colnames(IndicatorsFinalnum[f]),".png"), width=2000,height=700,pointsize=15)
+  boxplot(IndicatorsFinalnum[,f]~Targeted, data=IndicatorsFinalnum,xlab=paste(colnames(IndicatorsFinalnum[f]),names=paste0(Indicato$names, " (n=",b$n,")"))),lwd=3,cex.lab=2)
+  dev.off()
+}
+}
+
+nums=sapply(IndicatorsFinal,is.numeric)
+IndicatorsFinalnum=Ecoregionsubset[,nums]
+IndicatorsFinalnum$EcoregionHybrid10=Ecoregionsubset$EcoregionHybrid10
+IndicatorsFinalnum$Targeted=Ecoregionsubset$Targeted
+
+#for indicator by ecoregion/stream size
+for (f in 10:length(colnames(IndicatorsFinalnum))-2){
+  png(file=paste("boxplot",colnames(IndicatorsFinalnum[f]),".png"), width=2000,height=700,pointsize=10)
+  boxplot(IndicatorsFinalnum[,f]~Targeted, data=IndicatorsFinalnum,xlab=paste(colnames(IndicatorsFinalnum[f])),lwd=3,cex.lab=2)
+  dev.off()
+}
+
+
+#for indicator by ecoregion/stream size
+for (f in 10:length(colnames(IndicatorsFinalnum))){
+  png(file=paste("boxplot",colnames(IndicatorsFinalnum[f]),".png"), width=2000,height=700,pointsize=10)
+  boxplot(IndicatorsFinalnum[,f]~NAMC_Benchmark, data=IndicatorsFinalnum,xlab=paste(colnames(IndicatorsFinalnum[f])),lwd=3,cex.lab=2)
+  dev.off()
+}
+
+#for all indicators
+
+for (f in 10:length(colnames(IndicatorsFinalnum))){
+  png(file=paste("boxplot",colnames(IndicatorsFinalnum[f]),".png"), width=1000,height=700,pointsize=24)
+  boxplot(IndicatorsFinalnum[,f],xlab=paste(colnames(IndicatorsFinalnum[f])),lwd=3,cex.lab=2)
+  dev.off()
+}
+
+f <- function(x) {
+  r <- quantile(x, probs = c(0.05, 0.25, 0.5, 0.75, 0.95))
+  names(r) <- c("ymin", "lower", "middle", "upper", "ymax")
+  r
+}
+
+# define outlier as you want    
+o <- function(x) {
+  subset(x, x < quantile(x)[2] | quantile(x)[4] < x)
+}
+
+library(ggplot2)
+
+
+#####works!!!!
+
+IndicatorsFinal=read.csv("Z:\\buglab\\Research Projects\\AIM\\Workshops\\NAMF Data Analysis\\AK_2018\\BenchmarkScreeningDevelopment\\ALL_AK_data_for_workshop_16Feb2018_benchmark_boxplots_JC_exploration.csv")
+IndicatorsFinal=subset(IndicatorsFinal, IndicatorsFinal$Targeted=='Random')
+Indicators=c("NAMC_Benchmark","EcoregionHybrid10","OtherEcoregion","Targeted","BankCover","BankStability","BnkCover_Stab","BankfullWidth","BankOverheadCover","InstreamHabitatComplexity","FloodplainConnectivity","LWD_Freq","PctOverheadCover","pH","Slope","SpecificConductance","VegComplexity","PctFines","ThalwegDepthCV","Entrench")
+IndicatorsFinalsub=IndicatorsFinal[,Indicators]  
+IndicatorsFinalsub=subset(IndicatorsFinalsub, EcoregionHybrid10  %in% c('Alaska Range','Interior Highlands','Arctic Coastal Plain',"Interior Forested Lowlands and Uplands"))
+IndicatorsFinalsub=subset(IndicatorsFinalsub, EcoregionHybrid10  %in% c('Interior Highlands','Arctic Coastal Plain'))
+
+#IndicatorsFinalsub$EcoregionHybrid10=factor(IndicatorsFinalsub$EcoregionHybrid10, levels=levels(IndicatorsFinalsub$EcoregionHybrid10)[c(1,2,6,7)])
+IndicatorsFinalsub$NAMC_Benchmark=droplevels(IndicatorsFinalsub$NAMC_Benchmark)
+levels(IndicatorsFinalsub$NAMC_Benchmark)
+('Beaufort Coastal Plain','Alaska Range','Yukon-Tanana Uplands','Ray Mountains')
+for (s in 5:length(colnames(IndicatorsFinalsub))){
+  png(file=paste("boxplot",colnames(IndicatorsFinalsub[s]),".png"), width=2000,height=700,pointsize=20)
+  b=boxplot(IndicatorsFinalsub[,s]~NAMC_Benchmark*Targeted, data=IndicatorsFinalsub,xlab=paste(colnames(IndicatorsFinalsub[s])),lwd=3,cex.lab=2)
+  boxplot(IndicatorsFinalsub[,s]~NAMC_Benchmark*Targeted, data=IndicatorsFinalsub,xlab=paste(colnames(IndicatorsFinalsub[s])),names=paste0(b$names,"\n(n=",b$n,")"),lwd=3,cex.lab=2)
+   dev.off()
+}
+
+###########
+
+for (s in 4:length(Indicators)) {  
+ggplot(IndicatorsFinal, aes(EcoregionHybrid10,IndicatorsFinalsub[,5]))+ stat_summary(fun.data=f,geom="boxplot",fill='lightgray',colour="darkslategray") +
+  stat_summary(fun.y = o, geom="point")+
+  labs(x="", y = colnames(IndicatorsFinalsub[,5]))+
+  ylim(0.0,2.5)+
+  theme_classic()+
+  theme(axis.text=element_text(size=12),axis.title=element_text(size=12,face="bold") )#see http://ggplot2.tidyverse.org/reference/theme.html for more info on changing spacing and text size
+}
+
+
+#example new boxplot for vegComplexity
+#create paired reference and targeted boxplot
+png('VegComplex.png',width=1500, height=700, size=15)
+IndicatorsFinal=subset(IndicatorsFinal,OtherEcoregion %in% c('Beaufort Coastal Plain','Alaska Range','Yukon-Tanana Uplands','Ray Mountains'))
+  ggplot(IndicatorsFinal, aes(OtherEcoregion,VegComplexity))+ stat_summary(fun.data=f,geom="boxplot",fill='lightgray',colour="darkslategray") +
+  stat_summary(fun.y = o, geom="point")+
+  labs(x="", y = "VegComplexity")+
+ ylim(0.0,2.5)+
+  theme_classic()+
+  theme(axis.text=element_text(size=12),axis.title=element_text(size=12,face="bold") )#see http://ggplot2.tidyverse.org/reference/theme.html for more info on changing spacing and text size
+dev.off()
+
+png('FloodplainConnect.png',width=1500, height=700, size=15)
+  ggplot(IndicatorsFinal, aes(OtherEcoregion,FloodplainConnectivity))+ stat_summary(fun.data=f,geom="boxplot",fill='lightgray',colour="darkslategray") +
+    stat_summary(fun.y = o, geom="point")+
+    labs(x="", y = "Floodplain Connectivity")+
+    #ylim(0.0,0.5)+
+    theme_classic()+
+    theme(axis.text=element_text(size=12),axis.title=element_text(size=12,face="bold"),axis.text.x=element_blank(),axis.ticks.x=element_blank(),axis.title.x=element_blank() )#see http://ggplot2.tidyverse.org/reference/theme.html for more info on changing spacing and text size
+dev.off()
+    
+png('Entrench.png',width=1500, height=700, size=15)
+  ggplot(IndicatorsFinal, aes(OtherEcoregion,Entrench))+ stat_summary(fun.data=f,geom="boxplot",fill='lightgray',colour="darkslategray") +
+    stat_summary(fun.y = o, geom="point")+
+    labs(x="", y = "Entrench")+
+    #ylim(0.0,0.5)+
+    theme_classic()+
+    theme(axis.text=element_text(size=12),axis.title=element_text(size=12,face="bold") )#see http://ggplot2.tidyverse.org/reference/theme.html for more info on changing spacing and text size
+  dev.off()
+
+  
+  png('LWD_freq.png',width=1500, height=700, size=15)      
+  ggplot(IndicatorsFinal, aes(OtherEcoregion,LWD_Freq))+ stat_summary(fun.data=f,geom="boxplot",fill='lightgray',colour="darkslategray") +
+    stat_summary(fun.y = o, geom="point")+
+    labs(x="", y = "LWD_Freq")+
+    #ylim(0.0,0.5)+
+    theme_classic()+
+    theme(axis.text=element_text(size=12),axis.title=element_text(size=12,face="bold") )#see http://ggplot2.tidyverse.org/reference/theme.html for more info on changing spacing and text size
+  dev.off()
+  
+  png('PctFines.png',width=1500, height=700, size=15) 
+    ggplot(IndicatorsFinal, aes(OtherEcoregion,PctFines))+ stat_summary(fun.data=f,geom="boxplot",fill='lightgray',colour="darkslategray") +
+    stat_summary(fun.y = o, geom="point")+
+    labs(x="", y = "PctFines")+
+    ylim(0.0,2.5)+
+    theme_classic()+
+    theme(axis.text=element_text(size=12),axis.title=element_text(size=12,face="bold") )#see http://ggplot2.tidyverse.org/reference/theme.html for more info on changing spacing and text size
+    dev.off()
+    
+    png('PctOverheadCover.png',width=1500, height=700, size=15)    
+  ggplot(IndicatorsFinal, aes(OtherEcoregion,PctOverheadCover))+ stat_summary(fun.data=f,geom="boxplot",fill='lightgray',colour="darkslategray") +
+    stat_summary(fun.y = o, geom="point")+
+    labs(x="", y = "PctOverheadCover")+
+    #ylim(0.0,0.5)+
+    theme_classic()+
+    theme(axis.text=element_text(size=12),axis.title=element_text(size=12,face="bold"),axis.text.x=element_blank(),axis.ticks.x=element_blank(),axis.title.x=element_blank() )#see http://ggplot2.tidyverse.org/reference/theme.html for more info on changing spacing and text size
+  dev.off()
+  
+  png(file='boxplotpH.png', width=1500,height=700,pointsize=15)  
+  ggplot(IndicatorsFinal, aes(OtherEcoregion,pH))+ stat_summary(fun.data=f,geom="boxplot",fill='lightgray',colour="darkslategray") +
+    stat_summary(fun.y = o, geom="point")+
+    labs(x="", y = "pH")+
+    #ylim(0.0,0.5)+
+    theme_classic()+
+    theme(axis.text=element_text(size=12),axis.title=element_text(size=12,face="bold") )#see http://ggplot2.tidyverse.org/reference/theme.html for more info on changing spacing and text size
+  dev.off()
+  
+  png('Slope.png',width=1500, height=700, size=15)   
+  ggplot(IndicatorsFinal, aes(OtherEcoregion,Slope))+ stat_summary(fun.data=f,geom="boxplot",fill='lightgray',colour="darkslategray") +
+    stat_summary(fun.y = o, geom="point")+
+    labs(x="", y = "Slope")+
+    #ylim(0.0,0.5)+
+    theme_classic()+
+    theme(axis.text=element_text(size=12),axis.title=element_text(size=12,face="bold") )#see http://ggplot2.tidyverse.org/reference/theme.html for more info on changing spacing and text size
+  dev.off()  
+  
+  
+dev.off()
+
+
+
+
+
+
+boxplot(FloodplainConnectivity~NAMC_Benchmark,data=IndicatorsFinal)
+
+
+
+# # boxplot of Cal vs. Val OE scores:
+# b = boxplot(OE.assess.cal$OE.scores$OoverE, OE.assess.vld$OE.scores$OoverE,names=c("calibration","validation"),border=c("red", "blue"),ylab="O/E")
+# 
+# boxplot(OE.assess.cal$OE.scores$OoverE, OE.assess.vld$OE.scores$OoverE,border=c("red", "blue"),ylab="O/E",names=paste0(b$names, " (n=",b$n,")"))
+# abline(h=1.0,col=8,lty=3)
+# 
+#   png(file=paste("boxplot",".png"), width=1000,height=700,pointsize=10)
+#   par(mfrow=c(10,10))
+#   for (f in 10:length(colnames(IndicatorsFinalnum))){
+#   boxplot(IndicatorsFinalnum[,f],xlab=paste(colnames(IndicatorsFinalnum[f])),lwd=3,cex.lab=2)
+#   }
+# dev.off()  
