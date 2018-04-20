f629014862cc1efee9ce1248e417b2f51f3e4028
Jennifer Courtwright <jennifer.courtwright@usu.edu>
2018-04-20 15:28:49 -0600

    more playing around with loops

--- boxplot_loop_attempt_EPAdata.R
+++ boxplot_loop_attempt_EPAdata.R
@@ -1,22 +1,24 @@
 library(ggplot2)
 EPA_referencedata=read.csv('Z:\\buglab\\Research Projects\\BLM_WRSA_Stream_Surveys\\Results and Reports\\EPA_Data\\withoutdup_first_visit_for_boxplots.csv')
+EPA_referencedata=read.csv('withoutdup_first_visit_for_boxplots.csv')
+XE=EPA_referencedata[grep('^XE',EPA_referencedata$THRESH3_boxplot),]
 str(EPA_referencedata)
-unique(EPA_referencedata$Instream)
+unique(EPA_referencedata1$Instream)
 
-RiparianVariables=c("THRESH3_boxplot","THRESH2_boxplot","EcoregionHybrid10","InstreamHabitatComplexity","Pctfines","FloodplainConnectivity","LWD_Freq")
-#SedimentVariables=c("PctOverheadCover","BankOverheadCover","RiparianVegComplexity")
+#RiparianVariables=c("THRESH3_boxplot","THRESH2_boxplot","EcoregionHybrid10","InstreamHabitatComplexity","Pctfines","FloodplainConnectivity","LWD_Freq")
+SedimentVariables=c("THRESH3_boxplot","THRESH2_boxplot","EcoregionHybrid10","PctOverheadCover","BankOverheadCover","RiparianVegComplexity")
 RiparianVariables=c("NAMC_Benchmark","EcoregionHybrid10","InstreamHabitatComplexity","Pctfines","FloodplainConnectivity","LWD_Freq")
+EPA_referencedata1=subset(EPA_referencedata, Instream==1)
 
 
 
-EPA_referencedata1=EPA_referencedata[,RiparianVariables] 
-ecoregions=unique(unlist(EPA_referencedata1$EcoregionHybrid10))
+ecoregions=unique(unlist(EPA_referencedata$EcoregionHybrid10))
 
 for (s in 1:length(ecoregions)) {
   EPAsubset=subset(EPA_referencedata,EcoregionHybrid10==ecoregions[s])
    EPA_referencedata1=EPAsubset[,RiparianVariables] 
  for (f in 3:length(EPA_referencedata1)) { 
-   png(file=paste("boxplot",ecoregions[s],colnames(EPA_referencedata1[f]),".png"), width=1000,height=700,pointsize=24)
+   png(file=paste("boxplot",ecoregions[s],colnames(EPA_referencedata1[f]),".png"), width=1000,height=700,pointsize=10)
   boxplot(EPA_referencedata1[,f]~NAMC_Benchmark,data=EPAsubset)
    dev.off()
    }
@@ -35,14 +37,14 @@ for (s in 1:length(ecoregions)) {
 
 
 
+EPA_referencedata1=EPA_referencedata1[,SedimentVariables] 
+#par(mfrow=c(5,5))   
 
-  
-png(file=paste("boxplot.png"), width=1000,height=700,pointsize=4)
-par(mfrow=c(5,5)) 
- for (f in 3:length(EPA_referencedata1)) { 
-    boxplot(EPA_referencedata1[,f]~THRESH3_boxplot,data=EPA_referencedata1)
-  }
-  dev.off()
+ for (f in 4:length(EPA_referencedata1)) { 
+png(file=paste("boxplot",colnames(EPA_referencedata1[f]),".png"), width=3000,height=700,pointsize=13)
+boxplot(EPA_referencedata1[,f]~THRESH3_boxplot,data=EPA_referencedata1)
+  dev.off()  }
+
   
 boxplot.formula(EPA_referencedata1$InstreamHabitatComplexity)
 
@@ -58,7 +60,19 @@ for (f in 1:length(ResponseInfo2)){
   boxplot(ResponseInfo2[,f],xlab=paste(axislabels[f]),lwd=3,cex.lab=2,horizontal=TRUE)
   dev.off()
 }
+par("mfrow")
 
 epadata=read.csv("Z:\\buglab\\Research Projects\\BLM_WRSA_Stream_Surveys\\Results and Reports\\EPA_Data\\withoutdup_first_visit_for_boxplots.csv")
 str(epadata)
-boxplot(PctOverheadCover~NAMC_Benchmark, data=epadata)boxplot(PctOverheadCover~NAMC_Benchmark, data=epadata)boxplot(PctOverheadCover~NAMC_Benchmark, data=epadata)boxplot(PctOverheadCover~NAMC_Benchmark, data=epadata)boxplot(PctOverheadCover~NAMC_Benchmark, data=epadata)boxplot(RiparianVegComplexity~NAMC_Benchmark, data=epadata)boxplot(BankOverheadCover~NAMC_Benchmark, data=epadata)
+
+boxplot(PctOverheadCover~THRESH3_boxplot, data=EPA_referencedata1)
+boxplot(RiparianVegComplexity~THRESH3_boxplot, EPA_referencedata1)
+boxplot(BankOverheadCover~THRESH3_boxplot, data=EPA_referencedata1)
+
+png(file=)
+boxplot(InstreamHabitatComplexity~THRESH3_boxplot, data=EPA_referencedata1)
+boxplot(Pctfines~THRESH3_boxplot, data=EPA_referencedata1)
+boxplot(FloodplainConnectivity~THRESH3_boxplot, data=EPA_referencedata1)
+boxplot(LWD_Freq~THRESH3_boxplot, data=EPA_referencedata1)
+
+mHabitatComplexity","Pctfines","FloodplainConnectivity","LWD_Freq"
